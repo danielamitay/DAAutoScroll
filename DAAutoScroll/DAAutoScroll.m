087d84b5bec5d2fa301cf0e35101a12acbf8f847
danielamitay <hello@danielamitay.com>
2013-03-30 02:41:13 -0400

    Implement property methods

--- DAAutoScroll/DAAutoScroll.m
+++ DAAutoScroll/DAAutoScroll.m
@@ -8,6 +8,10 @@
 
 #import "UIScrollView+DA.h"
 
+static CGFloat UIScrollViewDefaultScrollPointsPerSecond = 15.0f;
+static char UIScrollViewScrollPointsPerSecondNumber;
+static char UIScrollViewAutoScrollTimer;
+
 @interface UIScrollView (DAAutoScroll_Internal)
 
 @property (nonatomic, strong) NSTimer *autoScrollTimer;
@@ -15,5 +19,64 @@
 @end
 
 @implementation UIScrollView (DAAutoScroll)
+
+#pragma mark - Property Methods
+
+- (void)setScrolling:(BOOL)scrolling
+{
+    if (scrolling)
+    {
+        //  Start scrolling
+    }
+    else
+    {
+        //  Stop scrolling
+    }
+}
+
+- (BOOL)scrolling
+{
+    return FALSE;
+}
+
+- (CGFloat)scrollPointsPerSecond
+{
+    NSNumber *scrollPointsPerSecondNumber = objc_getAssociatedObject(self,
+                                                                     &UIScrollViewScrollPointsPerSecondNumber);
+    if (scrollPointsPerSecondNumber)
+    {
+        return [scrollPointsPerSecondNumber floatValue];
+    }
+    else
+    {
+        return UIScrollViewDefaultScrollPointsPerSecond;
+    }
+}
+
+- (void)setScrollPointsPerSecond:(CGFloat)scrollPointsPerSecond
+{
+    [self willChangeValueForKey:@"scrollPointsPerSecond"];
+    objc_setAssociatedObject(self,
+                             &UIScrollViewScrollPointsPerSecondNumber,
+                             [NSNumber numberWithFloat:scrollPointsPerSecond],
+                             OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    [self didChangeValueForKey:@"scrollPointsPerSecond"];
+}
+
+- (NSTimer *)autoScrollTimer
+{
+    return objc_getAssociatedObject(self,
+                                    &UIScrollViewAutoScrollTimer);
+}
+
+- (void)setAutoScrollTimer:(NSTimer *)autoScrollTimer
+{
+    [self willChangeValueForKey:@"autoScrollTimer"];
+    objc_setAssociatedObject(self,
+                             &UIScrollViewAutoScrollTimer,
+                             autoScrollTimer,
+                             OBJC_ASSOCIATION_ASSIGN);
+    [self didChangeValueForKey:@"autoScrollTimer"];
+}
 
 @end
