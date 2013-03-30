38e8af7783d04373b12a663fce31184b2a0f45a9
danielamitay <hello@danielamitay.com>
2013-03-30 02:12:45 -0400

    Initial commit of overhaul

---
+++ DAAutoScroll/DAAutoScroll.h
@@ -0,0 +1,13 @@
+//
+//  DAAutoScroll.h
+//  DAAutoScroll
+//
+//  Created by Daniel Amitay on 3/30/13.
+//  Copyright (c) 2013 Daniel Amitay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (DAAutoScroll)
+
+@end
---
+++ DAAutoScroll/DAAutoScroll.m
@@ -0,0 +1,13 @@
+//
+//  DAAutoScroll.m
+//  DAAutoScroll
+//
+//  Created by Daniel Amitay on 3/30/13.
+//  Copyright (c) 2013 Daniel Amitay. All rights reserved.
+//
+
+#import "UIScrollView+DA.h"
+
+@implementation UIScrollView (DAAutoScroll)
+
+@end
